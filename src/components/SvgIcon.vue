aef8556e4266463ef34d722cb78fdd530cfeac9f
xy <3101116314@qq.com>
2024-05-03 19:05:05 +0800

    Svg组件的常规封装和h函数封装

---
+++ src/components/SvgIcon.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- 常规方式封装Svg组件 -->
+    <!-- <el-icon :size="size" :color="color">
+        <component :is="icon"/>
+    </el-icon> -->
+    <!-- h 函数封装Svg组件 -->
+    <render />
+</template>
+<script setup lang="ts">
+import { h, resolveComponent } from 'vue';
+
+// 通过ts约束父组件传递进来的数据 需要通过泛型来约束
+// withDefaults函数 就是对props的默认值进行设置 默认值在第二个参数中设置
+const props = withDefaults(defineProps<{
+    icon: string
+    color?: string
+    size?: string
+}>(), {
+    icon: "",
+    size: "18"
+})
+
+const render = () => {
+    if (props.icon) {
+        return h(resolveComponent("el-icon"), {
+            size: props.size,
+            color: props.color
+        }, [h(resolveComponent(props.icon))])
+    }
+}
+
+</script>
+<style lang='scss' scoped></style>
